0ffd2a5a91cc57dfcb84ad3957395879d10c5925
konekotech <89322889+konekotech@users.noreply.github.com>
2025-09-29 22:48:51 +0900

    feat ENQUESTII-5 Returnキーで入室

--- Assets/Scripts/Services/Pages/StartPage/StartMenu.cs
+++ Assets/Scripts/Services/Pages/StartPage/StartMenu.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            OnStartButton();
+        }
+    }
+
     public void OnStartButton()
     {
         SceneManager.LoadScene("SampleScene");
